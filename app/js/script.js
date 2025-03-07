05173bef5abaec7273088fdc729c51a62f361dce
undeadskeleton <safepeople@gmail.com>
2025-03-07 13:49:57 +0530

    fixing mobile margin and files input box

--- app/js/script.js
+++
@@ -1,9 +0,0 @@
-const dragDrop = document.querySelector(".dragdrop");
-const ddInput = dragDrop.getElementsByTagName("input");
-
-console.log("Linking sucessfull");
-
-dragDrop.addEventListener("click", () => {
-  //   alert("DragDrop is clicked");
-  ddInput.focus();
-});
